42b729aafb21927f35a583b4ddd345a22059af11
LeTung <user@USER-no-MacBook-Air.local>
2015-06-04 01:28:26 +0900

    boi vui

--- app/routes.php
+++ app/routes.php
@@ -10,39 +10,53 @@
 | and give it the Closure to execute when that URI is requested.
 |
 */
+Route::group([
+    "domain" => "musicfreeonline.tk"
+], function() {
+    Route::get ('/', 'HomeController@getIndex' );
 
-Route::get ('/', 'HomeController@getIndex' );
+    Route::get('/list','HomeController@getPlayList');
 
-Route::get('/list','HomeController@getPlayList');
+    Route::group(
+        [
+            'before' => '',
+            'prefix' => 'audios'
+        ]
+        , function() {
+            Route::get('/{audio_id}', 'AudiosController@getItem');
+            Route::get('/{audio_id}/{playlist_id}', 'AudiosController@playPlaylist');
+        });
 
-Route::group(
-    [
-        'before' => '',
-        'prefix' => 'audios'
-    ]
-    , function() {
-        Route::get('/{audio_id}', 'AudiosController@getItem');
-        Route::get('/{audio_id}/{playlist_id}', 'AudiosController@playPlaylist');
+    Route::group(
+        [
+            'prefix' => 'auth'
+        ]
+        , function() {
+            Route::get('login', 'AuthController@index');
+            Route::get('connect', 'AuthController@connect');
     });
 
-Route::group(
-    [
-        'prefix' => 'auth'
-    ]
-    , function() {
-        Route::get('login', 'AuthController@index');
-        Route::get('connect', 'AuthController@connect');
+    Route::group(
+        [
+            'before' => 'auth'
+        ]
+        , function() {
+            Route::resource('playlists', 'PlaylistController');
+            Route::get('me', 'AccountController@getIndex');
+            Route::post('bookmark/create', 'BookmarksController@postItem');
+            Route::get('audios/playlists', 'AudiosController@getPlaylists');
+            Route::get('auth/logout', 'AuthController@logout');
+        });
 });
 
-Route::group(
-    [
-        'before' => 'auth'
-    ]
-    , function() {
-        Route::resource('playlists', 'PlaylistController');
-        Route::get('me', 'AccountController@getIndex');
-        Route::post('bookmark/create', 'BookmarksController@postItem');
-        Route::get('audios/playlists', 'AudiosController@getPlaylists');
-        Route::get('auth/logout', 'AuthController@logout');
-    });
+Route::group([
+    "domain" => "boivuimienphi.tk"
+], function() {
+    Route::get ('/', 'BoiVui@index' );
 
+    Route::get ('/congviec', 'BoiVui@userInfoForm');
+
+    Route::post ('/ketqua', 'BoiVui@result');
+
+    Route::get ('/img', 'BoiVui@imageGenerator');
+});
