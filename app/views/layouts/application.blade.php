f60e455ca3403d6857399e2dcab5b5fd57ba5484
LeTung <user@USER-no-MacBook-Air.local>
2015-06-20 15:56:49 +0900

    no message

--- app/views/layouts/application.blade.php
+++ app/views/layouts/application.blade.php
@@ -4,10 +4,7 @@
     <meta charset="utf-8">
     <title>@yield('title', 'Music Streaming')</title>
       <meta name="viewport" content="width=device-width, user-scalable=no, initial-scale=1, maximum-scale=1">
-      <!-- Latest compiled and minified CSS -->
-      <link rel="stylesheet" href="//netdna.bootstrapcdn.com/bootstrap/3.1.1/css/bootstrap.min.css">
-      <!-- Optional theme -->
-      <link rel="stylesheet" href="https://bootswatch.com/cosmo/bootstrap.min.css">
+    {{ HTML::style('css/bootstrap.min.css') }}
     <link rel="stylesheet" href="{{ url('css/site.css')  }}">
       <script src="//ajax.googleapis.com/ajax/libs/jquery/1.11.1/jquery.min.js"></script>
       <!-- Latest compiled and minified JavaScript -->
